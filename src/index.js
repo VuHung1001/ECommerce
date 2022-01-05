7637fbac7c052024da5fdb8f399b2e58065461fe
VuHung1001 <hungvugithub@gmail.com>
2022-01-05 14:38:25 +0700

    run buildpack

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {store, persistor} from './redux/store'
 import {PersistGate} from 'redux-persist/integration/react'
 
 ReactDOM.render(
+  //redux
   <Provider store = {store}>
     <PersistGate loading={null} persistor={persistor}>
       <App />
