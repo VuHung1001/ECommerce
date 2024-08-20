cae6fb245d42e51a7fcb23590e465a727fa7aab5
hungvp <hungvp@ecoit.com.vn>
2024-08-20 17:18:40 +0700

    update vite config to build into /build folder and create separate chunks for each of dependencies

--- Client-ECommerce/vite.config.js
+++ Client-ECommerce/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: { 
+    outDir: 'build', // Specify the output directory here
+    rollupOptions: {
+      output: {
+        // This configuration will create separate chunks for each of your dependencies, which can significantly reduce the size of your main chunk.
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },  
 });
